6e83ce14226c34ce9f8ca9b8b154e6be32ee7f36
stone-w4tch3r <100294019+stone-w4tch3r@users.noreply.github.com>
2024-11-02 11:57:06 +0500

    vkui test projs added

---
+++ front-vkui/src/App.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  AdaptivityProvider,
+  ConfigProvider,
+  AppRoot,
+  SplitLayout,
+  SplitCol,
+  View,
+  Panel,
+  PanelHeader,
+  Header,
+  Group,
+  SimpleCell,
+} from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+const App = () => {
+
+  return (
+    <AppRoot>
+      <SplitLayout header={<PanelHeader delimiter="none" />}>
+        <SplitCol autoSpaced>
+          <View activePanel="main">
+            <Panel id="main">
+              <PanelHeader>VKUI</PanelHeader>
+              <Group header={<Header mode="secondary">Items</Header>}>
+                <SimpleCell>Hello</SimpleCell>
+                <SimpleCell>World</SimpleCell>
+              </Group>
+            </Panel>
+          </View>
+        </SplitCol>
+      </SplitLayout>
+    </AppRoot>
+  );
+};
+
+export default App;
